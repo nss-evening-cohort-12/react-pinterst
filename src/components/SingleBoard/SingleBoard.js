1ee297107260396d2ef1cb2d02da2a343ee19487
Luke Lancaster <luketlancaster@gmail.com>
2020-08-24 20:51:13 -0500

    Pin Create done!

--- src/components/SingleBoard/SingleBoard.js
+++ src/components/SingleBoard/SingleBoard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Pin from '../Pin/Pin';
+import PinForm from '../PinForm/PinForm';
 
 import boardsData from '../../helpers/data/boardsData';
 import pinsData from '../../helpers/data/pinsData';
@@ -15,6 +16,7 @@ class SingleBoard extends React.Component {
   state = {
     board: {},
     pins: [],
+    showForm: false,
   }
 
   goGetYoPins = () => {
@@ -43,19 +45,32 @@ class SingleBoard extends React.Component {
       .catch((err) => console.error('delete pin failed', err));
   }
 
+  createPin = (newPin) => {
+    pinsData.createPin(newPin)
+      .then(() => {
+        this.goGetYoPins();
+        this.setState({ showForm: false });
+      })
+      .catch((err) => console.error(err));
+  }
+
   render() {
-    const { board, pins } = this.state;
-    const { setSingleBoard } = this.props;
+    const { board, pins, showForm } = this.state;
+    const { setSingleBoard, boardId } = this.props;
 
     const pinCards = pins.map((pin) => <Pin key={pin.id} pin={pin} deletePin={this.deletePin}/>);
 
     return (
       <div>
         <h4>{board.name}</h4>
-        <button className="btn btn-danger" onClick={() => { setSingleBoard(''); }}>X</button>
+        <div className="mb-3">
+          <button className="btn btn-warning" onClick={() => { this.setState({ showForm: !showForm }); }}><i className={showForm ? 'far fa-times-circle' : 'far fa-plus-square'}></i></button>
+          {showForm ? <PinForm boardId={boardId} createPin={this.createPin} /> : ''}
+        </div>
         <div className="card-columns">
           {pinCards}
         </div>
+        <button className="btn btn-danger" onClick={() => { setSingleBoard(''); }}>Go Back</button>
       </div>
     );
   }
