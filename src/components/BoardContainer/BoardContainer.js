10783a528a1536c26216034cc2edbd01f4ba2e39
Luke Lancaster <luketlancaster@gmail.com>
2020-08-24 19:41:50 -0500

    Conditionally display new BoardForm on BoardContainer

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 import authData from '../../helpers/data/authData';
 import boardsData from '../../helpers/data/boardsData';
 import smashData from '../../helpers/data/smashData';
+
+// create board
+// BoardForm component
+// show BoardForm on click of some button in here
+  // need a button in here
+// on submit of form: save to firebase, make sure board shows up
 
 class BoardContainer extends React.Component {
   static propTypes = {
@@ -14,6 +21,7 @@ class BoardContainer extends React.Component {
 
   state = {
     boards: [],
+    formOpen: false,
   }
 
   getBoards = () => {
@@ -33,14 +41,18 @@ class BoardContainer extends React.Component {
   }
 
   render() {
-    const { boards } = this.state;
+    const { boards, formOpen } = this.state;
     const { setSingleBoard } = this.props;
 
     const boardCard = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard} deleteBoard={this.deleteBoard}/>);
 
     return (
-      <div className="card-columns">
-        {boardCard}
+      <div>
+        <button className="btn btn-warning" onClick={() => { this.setState({ formOpen: !formOpen }); }}><i className="far fa-plus-square"></i></button>
+        { formOpen ? <BoardForm /> : '' }
+        <div className="card-columns">
+          {boardCard}
+        </div>
       </div>
     );
   }
